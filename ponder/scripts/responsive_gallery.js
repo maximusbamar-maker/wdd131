5a2b1f675136bd3059a0e760d0e44a8b3a710788
Maximus Amar <maximusbamar@gmail.com>
2026-05-14 22:49:05 -0600

    responsive gallery

---
+++ ponder/scripts/responsive_gallery.js
@@ -0,0 +1,33 @@
+
+const gallery = document.querySelector('.gallery');
+const modal = document.querySelector('dialog');
+const modalImage = modal.querySelector('img');
+const closeButton = modal.querySelector('.close-viewer');
+
+// Event listener for opening the modal
+gallery.addEventListener('click', openModal);
+
+function openModal(event) {
+    // Code to show modal  - Use event parameter 'e'   
+    console.log(event);
+    let imageSrc = event.target.src;
+
+    // select img tag inside dialog, give it src
+    console.log(modalImage);
+    modalImage.src = imageSrc.replace("-sm", "-full");
+    console.log(typeof(imageSrc))
+    modal.showModal();
+}
+
+// Close modal on button click
+closeButton.addEventListener('click', () => {
+    modal.close();
+});
+
+// Close modal if clicking outside the image
+modal.addEventListener('click', (event) => {
+    if (event.target === modal) {
+        modal.close();
+    }
+});
+          
