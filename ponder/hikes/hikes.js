9c03e1532d6d2a7afb57db427a4f0fa019f7636c
Maximus Amar <maximusbamar@gmail.com>
2026-06-23 11:10:36 -0600

    Sorting

--- ponder/hikes/hikes.js
+++ ponder/hikes/hikes.js
@@ -1,5 +1,28 @@
-const names = ["James", "Bob", "Josh"]
+const people = [{name: "James", age: 33},
+                {name: "Bob",   age: 12},
+                {name: "Max",   age: 21},
+                {name: "Josh",  age: 47}];
 
-names.sort();
+people.sort(sortPeopleByName);
 
-confirm.log(names)
+console.log(people);
+
+// a custom sort function is needed to sort some things such as objects
+// a sort function can be any complexity but should return -1, 1, or 0 depending upon the conditions in it
+function sortPeopleByName(a, b) {
+    a_name = a.name.toLowerCase();
+    b_name = b.name.toLowerCase();
+
+    console.log("A " + a_name);
+    console.log("B " + b_name);
+
+    if(a_name < b_name) {
+        return -1; // left
+    }
+
+    else if (a_name > b_name){
+        return 1; // right
+    }
+
+    return 0; // stay
+}
